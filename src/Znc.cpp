2f1f25ed10fdbe182af604dd5e7b6b2817cd8604
kwakkel1000 <info@gijsweb.net>
2011-04-04 22:14:32 +0200

    added VHost add fix3

--- src/Znc.cpp
+++ src/Znc.cpp
@@ -343,6 +343,8 @@ void Znc::AddUser(std::string mChan, std::string mNick, std::string mAuth, std::
 			Send(returnstr);
 			returnstr = "PRIVMSG *admin :Set VHost " + mReqAuth + " " + Global::Instance().get_ConfigReader().GetString("znc_vhost") + "\r\n";
 			Send(returnstr);
+			returnstr = "PRIVMSG *admin :Set BindHost " + mReqAuth + " " + Global::Instance().get_ConfigReader().GetString("znc_vhost") + "\r\n";
+			Send(returnstr);
 			SaveConfig();
 			for (unsigned int AddUserText_it = 0; AddUserText_it < AddUserText.size(); AddUserText_it++)
 			{
